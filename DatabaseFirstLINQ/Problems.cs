a789e1039af6b2a80a5b6bfec5a54c364095ab29
Lizeth <mendezlizeth084@gmail.com>
2022-05-11 09:51:57 -0700

    Fixed Build Errors + Added SQL File + Solved Problem 1

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -15,7 +15,7 @@ namespace DatabaseFirstLINQ
         }
         public void RunLINQQueries()
         {
-            //ProblemOne();
+            ProblemOne();
             //ProblemTwo();
             //ProblemThree();
             //ProblemFour();
@@ -42,7 +42,12 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that returns the number of users in the Users table.
             // HINT: .ToList().Count
-
+            
+            var usersList = _context.Users.ToList().Count();
+            Console.WriteLine(usersList);
+       
+                
+           
         }
 
         private void ProblemTwo()
