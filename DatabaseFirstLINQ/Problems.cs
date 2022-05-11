053f67bedb6f6476db0286031441ca4da9506463
Lizeth <mendezlizeth084@gmail.com>
2022-05-11 14:53:03 -0700

    Problem 15 Previously Solved + Solved Problem 16

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -28,9 +28,9 @@ namespace DatabaseFirstLINQ
             //ProblemEleven();
             //ProblemTwelve();
             //ProblemThirteen();
-            ProblemFourteen();
+            //ProblemFourteen();
             //ProblemFifteen();
-            //ProblemSixteen();
+            ProblemSixteen();
             //ProblemSeventeen();
             //ProblemEighteen();
             //ProblemNineteen();
@@ -266,6 +266,11 @@ namespace DatabaseFirstLINQ
         private void ProblemSixteen()
         {
             // Update the price of the product you created to something different using LINQ.
+
+            var product = _context.Products.Where(p => p.Id == 8).SingleOrDefault();
+            product.Price = 350;
+            _context.Products.Update(product);
+            _context.SaveChanges();
 
         }
 
